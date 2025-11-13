4b2aa4d29f7aa9126138c4a8de895bbe0cccabeb
Noé Rivera Zevallos <85976759+myron98980@users.noreply.github.com>
2025-11-13 17:14:07 -0500

    Update RecentMovements.tsx

--- src/components/RecentMovements.tsx
+++ src/components/RecentMovements.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// Ya no necesitamos importar el ícono
+
 
 const UpArrowIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
