21511ec761dec68e83ffa9ef0e867723eebd6232
Noé Rivera Zevallos <85976759+myron98980@users.noreply.github.com>
2025-11-13 17:14:43 -0500

    Update UnderConstruction.tsx

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -1,15 +1,15 @@
 // src/components/UnderConstruction.tsx
 import React from 'react';
 
-// Importa el ícono de actualización
+
 import updateIcon from '../assets/update.png';
 
-// El componente recibirá una función 'onBack' para saber cómo regresar
+
 interface Props {
   onBack: () => void;
 }
 
-// Ícono de la flecha para el botón "Atrás"
+
 const BackArrowIcon = () => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
 );
